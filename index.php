c2e626102f2143c9f566cfeb0585b19f8bc3fccd
danielkwonwu <danielkwon.wu@gmail.com>
2020-01-02 00:03:27 -0600

    a

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
         <br>
         <br>
         <br>
-        <a href = "https://github.com/danielkwonwu"><img src="src/github.png" height = "36" width = "36"></a>  
-        <a href = "https://www.linkedin.com/in/daniel-kwon-3b5762177/"><img src="src/linkedin.png" height = "36" width = "36"></a>
+        <a id = "icon" href = "https://github.com/danielkwonwu"><img src="src/github.png" height = "36" width = "36"></a>  
+        <a id = "icon" href = "https://www.linkedin.com/in/daniel-kwon-3b5762177/"><img src="src/linkedin.png" height = "36" width = "36"></a>
     </div>
     <div class = "content">
         <h2> About Me </h2>
