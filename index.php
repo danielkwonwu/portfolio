9957dba45374ad2e34219db8f7f23b9c67d76763
danielkwonwu <danielkwon.wu@gmail.com>
2020-01-02 14:27:44 -0600

    a

--- index.php
+++ index.php
@@ -41,18 +41,37 @@
     <div class = "content">
         <h2 id = "current-page" >Projects</h2>
         <br>
+        <h3><a href = "https://github.com/danielkwonwu/gulp">GULP</a></h3>
+        <h4>Food Review Website with Geotag</h4>
+        <p>Technology Used: Express.js, React.js, Google Apis (Static Map, Geolocation)</p>
+        <p>Food Review website with functions including star rating visualization, near-by restaurant listing, SCSS, and more.</p>
         <br>
+
         <h3><a href = "https://github.com/danielkwonwu/k-means-uber-recommendation/blob/master/Uber%20Recommendation%20Report.pdf">k-Means Uber Recommendation</a></h3>
-        <p>Recommendation for Uber Vehicles’ Idle Location with Big Data Analysis</p>
+        <h4>Recommendation for Uber Vehicles’ Idle Location with Big Data Analysis</h4>
+        <p>Technology Used: Python, Spark, AWS E3</p>
+        <p>In the anticipation of the age of self-driving vehicles for hire, we've developed a k-means recommendation algorithm based on 4.5 million geolocation data points</p>
         <br>
         
         <h3><a href = "http://danielkwon.net:3456/">SimpleChatroom</a></h3>
-        <p>Node.js + Socket.io Multi-Room Environment Chatroom</p>
+        <h4>Multi-Room Environment Chatroom</h4>
+        <p>Technology Used: Socket.io, Node.js</p>
+        <p>Implementation of simple real-time chatroom with basic design and features.</p>
+        <br>
+
+        <h3><a href="https://github.com/khye99/user_alert">User_Alert Plugin</a></h3>
+        <h4>WordPress Plugin to Manage User Data</h4>
+        <p>Technology Used: PHP, WordPress, JSON formatting</p>
+        <p>A Wordpress plugin to crawl user data upon changes in user permission levels or via cron jobs and manual requests to a third-party URL. Data is sent in JSON format.</p>
         <br>
 
         <h3><a href= "http://danielkwon.net/~danielkwon/apptrac/">AppTrac</a></h3>
-        <p>PHP + MySQL Web App for Job Application Tracking</p>
+        <h4>Web App for Job Application Tracking</h4>
+        <p>Technology Used: PHP, MySQL, JavaScript</p>
+        <p>Job App Tracker with implementation of posting and editing features.</p>
         <br>
+
+        
 
     </div>
 </body>
