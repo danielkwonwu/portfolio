4399463da9ad05e207033fa98533f31cfea9981a
danielkwonwu <35541534+danielkwonwu@users.noreply.github.com>
2020-01-01 22:18:31 -0600

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,12 +1,19 @@
 <?php
 
-$file = 'src/resume1912.pdf';
 
-header ('Content-type : application/pdf');
-header ('Content-Disposition : inline; filename ="' . $file . '"');
-header ('Content-Transfer-Encoding : binary');
-header ('Accept-Range : bytes');
+$file = 'src/resume1219.pdf';
+$filename = 'src/resume1219.pdf';
 
+// Header content type
+header('Content-type: application/pdf');
+
+header('Content-Disposition: inline; filename="' . $filename . '"');
+
+header('Content-Transfer-Encoding: binary');
+
+header('Accept-Ranges: bytes');
+
+// Read the file
 @readfile($file);
 
 ?>
