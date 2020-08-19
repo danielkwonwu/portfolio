1d492681b50c3018084a0658eae33fae5e875e85
Daniel Kwon <danielkwon@wustl.edu>
2020-08-19 12:57:40 -0500

    asd

--- portfo/src/App.js
+++ portfo/src/App.js
@@ -1,76 +1,48 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Daniel Kwon Portfolio</title>
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Work+Sans:400,700" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="src/style.css">
-    
-</head>
-<body>
-    <div class = "menu">
-        <h2 id = "current-page" >About</h2>
-        <div style = "text-indent: 3%">
-            <img src="src/portrait.jpg" height = "320" weight = "320">
-        </div>
-            <h1><a href = "http://www.danielkwon.net/portfolio/">Daniel Kwon</a></h1>
-            <p>
-                I'm a senior at Wash U majoring in Computer Science and Political Science. 
-                I'm also an intern web developer for the Office of Public Affairs of Wash U. 
-                I am a fast learner and a passionate problem-solver coming from a background of experiences from different sectors.
-                I love learning from others and spreading what I know.
-                Please contact me at <a href = "danielkwon.wu@gmail.com">danielkwon.wu@gmail.com</a> if need arises.
-            </p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <h2><a href = "resume.php">Resume</a></h2>
-        <a href = "https://github.com/danielkwonwu"><img src="src/github.png" height = "30" width = "30"></a>  
-        <a href = "https://www.linkedin.com/in/daniel-kwon-3b5762177/"><img src="src/linkedin.png" height = "30" width = "30"></a>
-    </div>
-    
-    <div class = "content">
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (<div className = "content">
         <h2 id = "current-page" >Projects</h2>
-        <br>
+        
         <h3><a href = "https://github.com/danielkwonwu/gulp">GULP</a></h3>
         <h4>Food Review Website with Geotag</h4>
         <p><b>Technology Used</b>: Express.js, React.js, Google Apis (Static Map, Geolocation)</p>
         <p>Food Review website with functions including star rating visualization, near-by restaurant listing, SCSS, and more. Co-authored with Zach Young.</p>
-        <br>
 
         <h3><a href = "http://danielkwon.net/portfolio/my-treasure-no-touchy.html">My Treasure No Touchy!</a></h3>
         <h4>A Magical Tower Defense Game</h4>
         <p><b>Technology Used</b>: Unity, C#</p>
         <p>A tower-defense game with a wizard as the main character. Co-authored with Eric Li, Sam Wait, and Pranav Jain.</p>
-        <br>
 
         <h3><a href = "https://github.com/danielkwonwu/k-means-uber-recommendation/blob/master/Uber%20Recommendation%20Report.pdf">k-Means Uber Recommendation</a></h3>
         <h4>Recommendation for Uber Vehicles’ Idle Location with Big Data Analysis</h4>
         <p><b>Technology Used</b>: Python, Spark, AWS E3</p>
         <p>In the anticipation of the age of self-driving vehicles for hire, we've developed a k-means recommendation algorithm for the idle car locations based on 4.5 million geolocation data points. More information can be found in the report. Co-authored with Jice Kunfeng Zheng and Rui Qiu.</p>
-        <br>
         
         <h3><a href = "http://danielkwon.net:3456/">SimpleChatroom</a></h3>
         <h4>Multi-Room Environment Chatroom</h4>
         <p><b>Technology Used</b>: Socket.io, Node.js</p>
         <p>Implementation of simple real-time chatroom with basic design and features.  Co-authored with Zach Young.</p>
-        <br>
+        
 
         <h3><a href="https://github.com/khye99/user_alert">User_Alert Plugin</a></h3>
         <h4>WordPress Plugin to Manage User Data</h4>
         <p><b>Technology Used</b>: PHP, WordPress, JSON formatting</p>
         <p>A Wordpress plugin to crawl user data upon changes in user permission levels or via cron jobs and manual requests to a third-party URL. Data is sent in JSON format. Co-authored with Karen Ye.</p>
-        <br>
+        
 
         <h3><a href= "http://danielkwon.net/~danielkwon/apptrac/">AppTrac</a></h3>
         <h4>Web App for Job Application Tracking</h4>
         <p><b>Technology Used</b>: PHP, MySQL, JavaScript</p>
         <p>Job App Tracker with implementation of posting and editing features.</p>
-        <br>
-    </div>
-</body>
-</html>
+    </div>);
+  }
+}
 
+export default App;
